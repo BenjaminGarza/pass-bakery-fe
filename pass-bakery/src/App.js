250dfe50f9405e7f8a5dcad7bda1b016d8aa1197
benjamingarza <benjisgarza@gmail.com>
2022-11-01 21:08:22 -0700

    [Exercise-5] Pull out coffee section html

--- pass-bakery/src/App.js
+++ pass-bakery/src/App.js
@@ -15,35 +15,6 @@ function App() {
       </head>
       <body>
         <main>
-          <h2>Coffee</h2>
-          <ul>
-            <li>
-              <h3>Espresso</h3>
-              <br />
-              <p class="menu-text">$2.75</p>
-            </li>
-            <li>
-              <h3>Cappuccino</h3>
-              <br />
-              <p class="menu-text">$3.50</p>
-            </li>
-            <li>
-              <h3>Latte</h3>
-              <br />
-              <p class="menu-text">$4.00</p>
-            </li>
-            <li>
-              <h3>Drip</h3>
-              <br />
-              <p class="menu-text">$2.50</p>
-            </li>
-            <li>
-              <h3>Pour Over</h3>
-              <br />
-              <p class="menu-text">$4.75</p>
-            </li>
-            <li class="invisible"></li>
-          </ul>
           <h2>Breakfast:</h2>
           <ul>
             <li>
---
+++ pass-bakery/src/Coffee/Coffee.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export const Coffee = () => {
+  return (
+    <section>
+      <h2>Coffee</h2>
+      <ul>
+        <li>
+          <h3>Espresso</h3>
+          <br />
+          <p class="menu-text">$2.75</p>
+        </li>
+        <li>
+          <h3>Cappuccino</h3>
+          <br />
+          <p class="menu-text">$3.50</p>
+        </li>
+        <li>
+          <h3>Latte</h3>
+          <br />
+          <p class="menu-text">$4.00</p>
+        </li>
+        <li>
+          <h3>Drip</h3>
+          <br />
+          <p class="menu-text">$2.50</p>
+        </li>
+        <li>
+          <h3>Pour Over</h3>
+          <br />
+          <p class="menu-text">$4.75</p>
+        </li>
+        <li class="invisible"></li>
+      </ul>
+    </section>
+  );
+};
