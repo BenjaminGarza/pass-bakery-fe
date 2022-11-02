e6633b44c6e41be2a4ffa8e3717be1be25d5df4e
benjamingarza <benjisgarza@gmail.com>
2022-11-01 21:12:36 -0700

    [Exercise-5] Pull out Bread section html

--- pass-bakery/src/App.js
+++ pass-bakery/src/App.js
@@ -14,54 +14,7 @@ function App() {
         <title>PassBakery</title>
       </head>
       <body>
-        <main>
-          <h2>Breakfast:</h2>
-          <ul>
-            <li>
-              <h3>Oatmeal</h3>
-              <br />
-              <p class="menu-text">$4.00</p>
-            </li>
-            <li>
-              <h3>Toast</h3>
-              <br />
-              <p class="menu-text">$3.50</p>
-            </li>
-            <li>
-              <h3>Egg Sandwich</h3>
-              <br />
-              <p class="menu-text">$5.50</p>
-            </li>
-            <li>
-              <h3>Egg and Cheese Sandwich</h3>
-              <br />
-              <p class="menu-text">$6.50</p>
-            </li>
-          </ul>
-          <h2>Bread</h2>
-          <ul>
-            <li>
-              <h3>Pain de campagne</h3>
-              <br />
-              <p class="menu-text">$7.00</p>
-            </li>
-            <li>
-              <h3>Olive and lemon</h3>
-              <br />
-              <p class="menu-text">$7.50</p>
-            </li>
-            <li>
-              <h3>Brioche</h3>
-              <br />
-              <p class="menu-text">$8.00</p>
-            </li>
-            <li>
-              <h3>Ciabatta</h3>
-              <br />
-              <p class="menu-text">$5.50</p>
-            </li>
-          </ul>
-        </main>
+        <main></main>
       </body>
     </html>
   );
---
+++ pass-bakery/src/Bread/Bread.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Bread = () => {
+  return (
+    <section>
+      <h2>Bread</h2>
+      <ul>
+        <li>
+          <h3>Pain de campagne</h3>
+          <br />
+          <p class="menu-text">$7.00</p>
+        </li>
+        <li>
+          <h3>Olive and lemon</h3>
+          <br />
+          <p class="menu-text">$7.50</p>
+        </li>
+        <li>
+          <h3>Brioche</h3>
+          <br />
+          <p class="menu-text">$8.00</p>
+        </li>
+        <li>
+          <h3>Ciabatta</h3>
+          <br />
+          <p class="menu-text">$5.50</p>
+        </li>
+      </ul>
+    </section>
+  );
+};
