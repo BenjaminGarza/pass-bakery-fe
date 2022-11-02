e9bb694c21fe39408aeea918c65ad6c93d8b891a
benjamingarza <benjisgarza@gmail.com>
2022-11-01 21:10:36 -0700

    [Exercise-5] Pull out Breakfast section html

---
+++ pass-bakery/src/Breakfast/Breakfast.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Breakfast = () => {
+  return (
+    <section>
+      <h2>Breakfast:</h2>
+      <ul>
+        <li>
+          <h3>Oatmeal</h3>
+          <br />
+          <p class="menu-text">$4.00</p>
+        </li>
+        <li>
+          <h3>Toast</h3>
+          <br />
+          <p class="menu-text">$3.50</p>
+        </li>
+        <li>
+          <h3>Egg Sandwich</h3>
+          <br />
+          <p class="menu-text">$5.50</p>
+        </li>
+        <li>
+          <h3>Egg and Cheese Sandwich</h3>
+          <br />
+          <p class="menu-text">$6.50</p>
+        </li>
+      </ul>{" "}
+    </section>
+  );
+};
